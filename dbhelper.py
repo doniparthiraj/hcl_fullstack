2e29109818094a2792a102acfc7a41e6efe53e6a
doniparthiraj <61642899+doniparthiraj@users.noreply.github.com>
2025-03-01 14:56:43 +0530

    Update dbhelper.py raj3

--- dbhelper.py
+++ dbhelper.py
@@ -21,15 +21,16 @@ class DBHelper:
         id INT AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(100) NOT NULL,
         phonenumber VARCHAR(100) UNIQUE NOT NULL,
-        bank_name VARCHAR(100) NOT NULL,
-        amount INT
+        accountnumber VARCHAR(100) UNIQUE NOT NULL,
+        bic VARCHAR(100) NOT NULL,
+        amount INT,
+        FOREIGN KEY(bic) references bank_details(bic)
         )
         """)
         conn.commit()
         cursor.execute("""
         CREATE TABLE IF NOT EXISTS bank_details(
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
+        bic VARCHAR(100) PRIMARY KEY,
         charges INT
         )
         """)
@@ -38,7 +39,9 @@ class DBHelper:
         CREATE TABLE IF NOT EXISTS bank_links(
         from_bic VARCHAR(100) NOT NULL,
         to_bic VARCHAR(100) NOT NULL,
-        time INT
+        time INT,
+        FOREIGN KEY(from_bic) references bank_details(bic),
+        FOREIGN KEY(to_bic) references bank_details(bic)
         )
         """)
         conn.commit()
@@ -46,12 +49,13 @@ class DBHelper:
     def add_user(self,param):
         conn = self.get_db_connection()
         cursor = conn.cursor()
-        name = param.get("user")
+        name = param.get("name")
         phonenumber = param.get("phonenumber")
-        bank_name = param.get("bank_name")
+        bic = param.get("bic")
         amount = param.get("amount")
-        query = "INSERT INTO users (name,phonenumber,bank_name,amount) VALUES(%s,%s,%s,%s)"
-        values = (name,phonenumber,bank_name,amount)
+        accountnumber = param.ge("accountnumber")
+        query = "INSERT INTO users (name,phonenumber,bic,amount) VALUES(%s,%s,%s,%s,%s)"
+        values = (name,phonenumber,accountnumber,bic,amount)
         try:
             cursor.execute(query,values)
             conn.commit()
@@ -65,9 +69,9 @@ class DBHelper:
     def add_bank_details(self,param):
         conn = self.get_db_connection()
         cursor = conn.cursor()
-        bank_name = param.get("bank_name")
+        bank_name = param.get("bic")
         charges = param.get("charges")
-        query = "INSERT INTO bank_details (bank_name,charges) VALUES(%s,%s)"
+        query = "INSERT INTO bank_details (bic,charges) VALUES(%s,%s)"
         values = (bank_name,charges)
         try:
             cursor.execute(query,values)
@@ -97,3 +101,48 @@ class DBHelper:
             cursor.close()
             conn.close()
     
+    def get_links(self):
+        conn = self.get_db_connection()
+        cursor = conn.cursor()
+        query = "select from_bic,to_bic,time from bank_links"
+        try:
+            cursor.execute(query)
+            response = cursor.fetchall()
+            return response
+        except mysql.connector.IntegrityError:
+            return []
+        finally:
+            cursor.close()
+            conn.close()
+
+    def get_charges(self):
+        conn = self.get_db_connection()
+        cursor = conn.cursor()
+        query = "select bic,charges from bank_details"
+        try:
+            cursor.execute(query)
+            response = cursor.fetchall()
+            return response
+        except mysql.connector.IntegrityError:
+            return []
+        finally:
+            cursor.close()
+            conn.close()
+
+    def transfer(self,param,amount):
+        conn = self.get_db_connection()
+        cursor = conn.cursor()
+        value1 = (param.get("from_acc"),amount)
+        value2 = (param.get("to_acc"),amount)
+        query1 = "update amount set amount = (amount + %s) where accountnumber = %s"
+        query2 = "update amount set amount = (amount - %s) where accountnumber = %s"
+        try:
+            cursor.execute(query1,value1)
+            cursor.execute(query2,value2)
+            conn.commit()
+            return {"message":"succesfully transfered amount"}, 201
+        except mysql.connector.IntegrityError:
+            return {"error": "error in transfering!"}, 400
+        finally:
+            cursor.close()
+            conn.close()
