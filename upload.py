e98cab5a0777559f646cc37a12448d12c54ff897
doniparthiraj <61642899+doniparthiraj@users.noreply.github.com>
2025-03-01 14:07:40 +0530

    Add files via upload

---
+++ upload.py
@@ -0,0 +1,18 @@
+import networkx as nx
+import pandas as pd
+from dbhelper import db_helper
+
+csv_file = "banks.csv"
+df = pd.read_csv(csv_file)
+
+
+for _, row in df.iterrows():
+    db_helper.add_bank_details({"bank_name":row["BIC"], "charges":row["Charge"]})
+
+csv_file = "links.csv"
+df = pd.read_csv(csv_file)
+
+
+for _, row in df.iterrows():
+    db_helper.link_bank({"from_bic" :row["FromBIC"], "to_bic":row["ToBIC"], "time":row["TimeTakenInMinutes"]})
+
